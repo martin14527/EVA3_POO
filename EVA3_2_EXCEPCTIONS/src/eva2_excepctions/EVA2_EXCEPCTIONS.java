22083de8e8f3afd283c9748d86bc1fda1f78a540
martin14527 <guillermovallejoloya@gmail.com>
2026-05-06 22:41:24 -0600

    Add files via upload

---
+++ EVA3_2_EXCEPCTIONS/src/eva2_excepctions/EVA2_EXCEPCTIONS.java
@@ -0,0 +1,16 @@
+
+package eva2_excepctions;
+
+public class EVA2_EXCEPCTIONS {
+    public static void main(String[] args) {
+      
+         int pe = 10, y=0; int resultado;
+    
+   // resultado=pe/y; //como no se puede dividir eso, es una excepcion
+    System.out.println("resultaedo"); 
+    
+    int[] arreglo = new int[5];
+    System.out.println(arreglo[6]); //no existe una posicion 6 
+    }
+    
+}
